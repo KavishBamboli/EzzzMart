5117e39945e6768e61e3041f6c45b0c3192023bd
Kavish Bamboli <bambolikavish20@gmail.com>
2022-03-24 23:25:05 +0530

    Added username connectivity

--- ServerFiles/GetSalesData.php
+++ ServerFiles/GetSalesData.php
@@ -2,7 +2,7 @@
 
 	header('Access-Control-Allow-Origin: *');
 
-	$mysqli = new mysqli("localhost", "root", "kavish1234", "phppractice");
+	$mysqli = new mysqli("localhost", "root", "kavish1234", "ezzzmart");
 	if($mysqli->connect_error) {
 		exit('Could not connect');
 	}
--- ServerFiles/GetUserDetails.php
+++ ServerFiles/GetUserDetails.php
@@ -1,3 +1,22 @@
 <?php
-	 
+	header('Access-Control-Allow-Origin: *');
+
+	$mysqli = new mysqli("localhost", "root", "kavish1234", "ezzzmart");
+	if($mysqli->connect_error) {
+		exit('Could not connect');
+	}
+
+	$sql = "SELECT Username FROM users WHERE UserId = ?";
+
+	$stmt = $mysqli->prepare($sql);
+	$stmt->bind_param("i", $_GET['userId']);
+	$stmt->execute();
+	$stmt->store_result();
+	$stmt->bind_result($username);
+	$stmt->fetch();
+	$stmt->close();
+
+	$arr = array('UserName' => $username);
+
+	echo json_encode($arr); 
 ?>
