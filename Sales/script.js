c923f56ef96ac5f2b2ed28efdd716665ed1e20ee
Kavish Bamboli <bambolikavish20@gmail.com>
2022-03-24 23:10:28 +0530

    Added connectivity

--- Sales/script.js
+++ Sales/script.js
@@ -1,6 +1,69 @@
-var date = document.getElementsByClassName("date");
+var startDate = document.getElementById("startDate");
+var endDate = document.getElementById("endDate");
+var searchSaleId = document.getElementById("search");
+var salesDetails = document.getElementById("sales-details");
 
-for (var i = 0; i < date.length; i++) {
-	var today = new Date();
-	date[i].value = today.toISOString().substr(0, 10);
+var today = new Date();
+startDate.value = today.toISOString().substr(0, 10);
+endDate.value = today.toISOString().substr(0, 10);
+
+function executeSearch() {
+	if(searchSaleId.value == '')
+	{
+		if(endDate.value > today.toISOString().substr(0, 10))
+		{
+			alert("End date cannot be greater than today");
+			endDate.value = today.toISOString().substr(0, 10);
+		}
+		else if(startDate.value > endDate.value)
+		{
+			alert("Start date cannot be greater than or equal to the end date");
+			startDate.value = today.toISOString().substr(0, 10);
+			endDate.value = today.toISOString().substr(0, 10);
+		}
+		else
+		{
+			// Call php file to get data
+			const xhttp = new XMLHttpRequest();
+			xhttp.onload = function() {
+				var responseObj = JSON.parse(this.responseText);
+			}
+			xhttp.open("GET", "http://localhost/EzzzMart/ServerFiles/GetSalesData.php?searchType=date&startDate="+startDate.value+"&endDate="+endDate.value+"");
+			xhttp.send();
+			salesDetails.style.display = "block";
+		}
+	}
+	else
+	{
+		const xhttp = new XMLHttpRequest();
+		xhttp.onload = function() {
+		   salesDetails.innerHTML = this.responseText;
+		}
+		xhttp.open("GET", "http://localhost/EzzzMart/ServerFiles/GetSalesData.php?searchType=date&saleId="+searchSaleId.value+"");
+		xhttp.send();
+		salesDetails.style.display = "block";
+	}
+}
+
+function getSalesTarget() {
+	// Get Sales target from database
+}
+
+function updateProgressBar() {
+	var totalMonthlySales = 0;
+	var salesTarget = getSalesTarget();
+	var month = today.getMonth() + 1;
+	console.log(month);
+
+	var salesData = null; // get data from database;
+	for (var i = 0; i < salesData.length; i++) {
+		totalMonthlySales += salesData[i]['SalesAmount'];
+	}
+
+	var progressBar = document.getElementById('progress-bar')
+	progressBar.value = totalMonthlySales / salesTarget * 100;
+}
+
+function displayRecords() {
+	
 }
---
+++ ServerFiles/GetSalesData.php
@@ -0,0 +1,24 @@
+<?php
+
+	header('Access-Control-Allow-Origin: *');
+
+	$mysqli = new mysqli("localhost", "root", "kavish1234", "phppractice");
+	if($mysqli->connect_error) {
+		exit('Could not connect');
+	}
+	
+	$sql = "SELECT SaleId, Date, Customer_Name, BillAmount, OnlineSale, BilledBy
+			FROM sales WHERE Date between ? and ?";
+
+	$stmt = $mysqli->prepare($sql2);
+	$stmt->bind_param("ss", $_GET['startDate'], $_GET['endDate']);
+	$stmt->execute();
+	$stmt->store_result();
+	$stmt->bind_result($saleId, $date, $cname, $billAmt, $onlineSale, $billedBy);
+	$stmt->fetch();
+	$stmt->close();
+
+	$arr = array('SaleId' => $saleId, 'Date' => $date, 'CustomerName' => $cname, 'BillAmount' => $billAmt, 'OnlineSale' => $onlineSale, 'BillAmt' => $billedBy);
+
+	echo json_encode($arr);
+?>
